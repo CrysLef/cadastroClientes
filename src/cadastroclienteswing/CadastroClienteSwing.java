f8d83feb6e6432f52f02349f76626ca5574b0afd
Crystian Lefundes <crystian.lf@gmail.com>
2024-01-28 08:10:31 -0300

    Adicionando tela de formulario com a logica

---
+++ src/cadastroclienteswing/CadastroClienteSwing.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastroclienteswing;
+
+/**
+ *
+ * @author crys
+ */
+public class CadastroClienteSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        var tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+    
+}
